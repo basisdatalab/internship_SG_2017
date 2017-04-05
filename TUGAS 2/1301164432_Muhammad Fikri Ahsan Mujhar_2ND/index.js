166633ccf796d14bdae7b6d6a95609265f111651
Fikri Aksan <Fikri Aksan>
2017-04-05 23:22:08 +0700

    Jurnal 2 First Push

---
+++ TUGAS 2/1301164432_Muhammad Fikri Ahsan Mujhar_2ND/index.js
@@ -0,0 +1,7 @@
+function register(){
+	alert("Hai " + $('#username').val() + " dengan Password " + $('#pwd').val() + ", Anda Telah Terdaftar");
+}
+
+function login(){
+	alert("Halo " + $('#username').val() + " , Anda Telah Login");
+}
