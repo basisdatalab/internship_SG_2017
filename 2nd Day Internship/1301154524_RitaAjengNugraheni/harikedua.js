090d92685d2d487a77eaaa344cfaa4f41ce4f44e
RITA AJENG NUGRAHENI <RITA AJENG NUGRAHENI>
2017-04-06 17:56:39 +0700

    commit pertama tugas internship

---
+++ 2nd Day Internship/1301154524_RitaAjengNugraheni/harikedua.js
@@ -0,0 +1,8 @@
+/*	alert("hai" + $('#input1'),val()+"dengan pasword"+$ ('#input2').val() + "anda telah terdaftar");
+}
+$('#button'), click(function(){
+	alert("hai");
+});*/
+alert("HAI INI TUGAS INTERNSHIP");
+
+/*MASI BINGUNG KENAPA FUNGSINYA GA MAU JALAN */
