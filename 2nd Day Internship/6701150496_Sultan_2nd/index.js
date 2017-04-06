4516954528ca24cc52bed7110329e91a1e5e5420
sultanhpk <sultanputerak@gmail.com>
2017-04-06 19:51:24 +0700

    add jurnal hari kedua

---
+++ 2nd Day Internship/6701150496_Sultan_2nd/index.js
@@ -0,0 +1,6 @@
+function klikLogin(){
+	alert("Halo "+$("#username").val()+" anda telah login");
+}
+function klikRegister(){
+	alert("Hai "+$("#username").val()+" dengan password "+$("#pwd").val()+" anda telah terdaftar");
+}
