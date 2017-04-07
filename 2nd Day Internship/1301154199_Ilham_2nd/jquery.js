b2a1c6bc0297713adbcc23b0cb51c87d7b307bc4
ilham <ilhamkurnia1711@gmail.com>
2017-04-08 00:46:12 +0700

    tugas

---
+++ 2nd Day Internship/1301154199_Ilham_2nd/jquery.js
@@ -0,0 +1,17 @@
+function alert1(){
+	user = $("#01").val();
+	alert("Halo "+ user + ". Anda berhasil login.");
+
+}
+
+/*function alert2(){
+	user = $("#btn1").val()
+	alert("Halo "+ user + ". Anda berhasil regis.")
+
+}*/ 
+
+function alert3(){
+	haha1 =$("#btn2").val()
+	haha2 =$("#btn3").val()
+	alert("Username : "+haha1+ " Password : "+haha2)
+}
