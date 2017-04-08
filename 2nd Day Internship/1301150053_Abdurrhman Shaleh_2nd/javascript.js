1ff554bb3895c468920e9780b73acfa45e45c7d9
Abdusha <shaleh2105@gmail.com>
2017-04-08 20:19:56 +0700

    {abdurrahman shaleh} Jurnal 2

---
+++ 2nd Day Internship/1301150053_Abdurrhman Shaleh_2nd/javascript.js
@@ -0,0 +1,14 @@
+function klikL(){
+	isiUser = $("#username").val();
+	isiPass = $("#pass").val();
+
+	alert("Selamat " + isiUser + ", anda berhasil Login ^_^")
+}
+
+function klikR(){
+	isiUser = $("#username").val();
+	isiEmail = $("#email").val();
+	isiPass = $("#pass").val();
+
+	alert("Hallo " + isiUser + ", anda telah terdaftar dengan email : " + isiEmail + "dan password : " + isiPass )
+}
