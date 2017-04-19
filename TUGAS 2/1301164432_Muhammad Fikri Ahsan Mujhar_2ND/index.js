9c0728f5bb706ccc2c8e75982e7b9f1cd5fe1b27
Helmi <satriahelmi@gmail.com>
2017-04-19 09:37:30 +0700

    beautify folder

--- TUGAS 2/1301164432_Muhammad Fikri Ahsan Mujhar_2ND/index.js
+++
@@ -1,7 +0,0 @@
-function register(){
-	alert("Hai " + $('#username').val() + " dengan Password " + $('#pwd').val() + ", Anda Telah Terdaftar");
-}
-
-function login(){
-	alert("Halo " + $('#username').val() + " , Anda Telah Login");
-}
